f845bbcf022da441fee9aa10bcc71311a53fce84
Yuri Donato <yuridonatosis@gmail.com>
2024-09-05 18:11:45 -0300

    terst

--- api/app.py
+++ api/app.py
@@ -3,11 +3,10 @@ from flask_cors import CORS
 from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
+import cloudscraper
 
 app = Flask(__name__)
-
-# Habilitar o CORS para permitir requisições do React
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Rota para membros
 @app.route('/members')
@@ -17,7 +16,6 @@ def members():
 # Rota para hora atual
 @app.route('/hora')
 def hora():
-    # Retorna a hora atual com segundos
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     return jsonify({'hora': current_time})
@@ -32,7 +30,6 @@ def scrape_economic_events():
     soup = BeautifulSoup(page.content, 'html.parser')
 
     events = []
-
     table = soup.find('table', id='economicCalendarData')
     rows = table.find_all('tr', class_='js-event-item')
 
@@ -70,5 +67,35 @@ def get_events_by_currency(currency):
     filtered_events = [event for event in events if event['moeda'] == currency.upper()]
     return jsonify(filtered_events)
 
+# Função para fazer o scraping do preço de BTC/USD
+def scrape_btc_usd_price():
+    url = 'https://br.investing.com/crypto/bitcoin/btc-usd'
+    scraper = cloudscraper.create_scraper()  # cloudscraper é compatível com Cloudflare
+    
+    try:
+        page = scraper.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        # Procurando o elemento que contém o preço de BTC/USD
+        price_div = soup.find('div', {'data-test': 'instrument-price-last'})
+        if price_div:
+            price = price_div.text.strip()
+            return price
+        else:
+            return None
+    except Exception as e:
+        print(f"Erro ao capturar o preço: {e}")
+        return None
+
+# Rota para obter o preço atual do BTC/USD
+@app.route('/market/btcusd/currentprice', methods=['GET'])
+def current_btc_usd_price():
+    price = scrape_btc_usd_price()
+    
+    if price:
+        return jsonify({'price': price})
+    else:
+        return jsonify({'error': 'Price not found'}), 404
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
