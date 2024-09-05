bbf713049c85b0234f05ba0069a50e460fb5790e
Yuri Donato <yuridonatosis@gmail.com>
2024-09-05 18:13:57 -0300

    test

--- api/members.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-bp = Blueprint('members', __name__)
-
-@bp.route('/members', methods=['GET'])
-def members():
-    return jsonify({'members': ['member1', 'member2', 'member34']})
